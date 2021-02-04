dc34fed0ef5ab494bf5e203e2858e7a27723e394
zhangyang1990 <359102039@qq.com>
2021-02-04 16:17:12 +0800

    BHOne

---
+++ BHTest/BHTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BHTest
+//
+//  Created by Yang on 2021/2/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BHTest/BHTest/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  BHTest
+//
+//  Created by Yang on 2021/2/4.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
